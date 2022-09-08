ad47a8aa9809320f43c693c856d7710a0abfdd2c
Anders Clausen <anders.s.clausen@gmail.com>
2022-09-08 11:26:37 +0100

    Initial upload

---
+++ src/main/java/com/example/invalidpathissue/InvalidPathIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.invalidpathissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvalidPathIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvalidPathIssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/invalidpathissue/InvalidPathIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invalidpathissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvalidPathIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
